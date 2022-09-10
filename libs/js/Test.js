be0c82dde37cd46189a90c704a809e234485b406
CGIHUANG001 <cgihuang@sina.com>
2022-09-11 00:47:09 +0800

    Update Test.js

--- libs/js/Test.js
+++ libs/js/Test.js
@@ -4,7 +4,7 @@ import { OrbitControls } from '../jsm/controls/OrbitControls.js';
 import { FBXLoader } from '../jsm/loaders/FBXLoader.js';
 import { RGBELoader } from '../jsm/loaders/RGBELoader.js';
 
-console.log("0.32"); 
+console.log("0.33"); 
 
 // ■ html div
 const container = document.getElementById("container");
