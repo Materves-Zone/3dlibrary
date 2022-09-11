5ea479b9daefc4e5b815a1bab668e35f03d02fcf
CGIHUANG001 <cgihuang@sina.com>
2022-09-11 11:50:17 +0800

    Update Test01.js

--- libs/js/Test01.js
+++ libs/js/Test01.js
@@ -1,13 +1,19 @@
-console.log("0.12");
+console.log("0.13");
 
 function myBrowser () {
     
-    if(window.navigator.userAgent.split("safari") != null){
-        alert("Safari Browser");
-    }
-    else if(window.navigator.userAgent.split("chorme") != null){
-        alert("Chorme Browser");
-    }
+    var Broswer = window.navigator.userAgent;
+    console.log(Broswer);
+    var BrowserVerName = window.navigator.userAgent.split('/');
+    console.log(BrowserVerName[BrowserVerName.length-2]);
+    console.log(BrowserVerName[1]);
+    var BroswerName = BrowserVerName[BrowserVerName.length-2].split(' ');
+    console.log(BroswerName[BroswerName.length-1]);
+    var BrowserPlatform = BrowserVerName[1].split(' ');
+    console.log(BrowserPlatform[1]);
+    alert("This is " + BrowserPlatform[1] + "  with  " + BroswerName[BroswerName.length-1] + "  Browser!");
+
+
 }
 
 myBrowser();
