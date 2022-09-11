7e8acb0f15c3378c55b1872d9d6ee2f18920c495
CGIHUANG001 <cgihuang@sina.com>
2022-09-11 12:05:34 +0800

    Update Test01.js

--- libs/js/Test01.js
+++ libs/js/Test01.js
@@ -3,16 +3,16 @@ console.log("0.13");
 function myBrowser () {
     
     var Broswer = window.navigator.userAgent;
-    console.log(Broswer);
-    var BrowserVerName = window.navigator.userAgent.split('/');
-    console.log(BrowserVerName[BrowserVerName.length-2]);
-    console.log(BrowserVerName[1]);
-    var BroswerName = BrowserVerName[BrowserVerName.length-2].split(' ');
-    console.log(BroswerName[BroswerName.length-1]);
-    var BrowserPlatform = BrowserVerName[1].split(' ');
-    console.log(BrowserPlatform[1]);
-    alert("This is " + BrowserPlatform[1] + "  with  " + BroswerName[BroswerName.length-1] + "  Browser!");
-
+    alert(Broswer);
+    // console.log(Broswer);
+    // var BrowserVerName = window.navigator.userAgent.split('/');
+    // console.log(BrowserVerName[BrowserVerName.length-2]);
+    // console.log(BrowserVerName[1]);
+    // var BroswerName = BrowserVerName[BrowserVerName.length-2].split(' ');
+    // console.log(BroswerName[BroswerName.length-1]);
+    // var BrowserPlatform = BrowserVerName[1].split(' ');
+    // console.log(BrowserPlatform[1]);
+    // alert("This is " + BrowserPlatform[1] + "  with  " + BroswerName[BroswerName.length-1] + "  Browser!");
 
 }
 
